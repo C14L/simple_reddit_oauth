63bc5dd9ae0af8c54bf58baacd33215b9048e274
Christian Steimel <me@C14L.com>
2015-10-18 12:25:59 +0200

    add names to URL defnitions

--- urls.py
+++ urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'login/', views.login_view),
-    url(r'redditcallback/', views.reddit_callback_view),
-    url(r'logout/', views.logout_view),
+    url(r'login/', views.login_view, name='logout_page'),
+    url(r'redditcallback/', views.reddit_callback_view, name='redditcb_page'),
+    url(r'logout/', views.logout_view, name='logout_page'),
 ]
